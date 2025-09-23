0673008cb9f729ca18e1083214b5d3de49f70e66
amy-maria <122695916+amy-maria@users.noreply.github.com>
2025-09-23 14:24:14 -0400

    carousel slides w/swiper fixed

--- arbusiness/src/app/ui/carousel.tsx
+++ arbusiness/src/app/ui/carousel.tsx
@@ -1,27 +1,39 @@
 'use client'
 import Image from 'next/image';
 import {Swiper, SwiperSlide} from 'swiper/react';
-import {Navigation, Scrollbar, A11y} from 'swiper';
+import {Navigation, Scrollbar, A11y} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
 //import { builtinModules } from 'module';
 
 export default function Carousel({ slides }: { slides: { sourceUrl: string; altText: string }[] }) {
   if (!slides || !slides.length) return null;
+ 
   return (
     <Swiper 
     modules={[Navigation, Scrollbar, A11y]}
     spaceBetween={30} 
-    slidesPerView={2} 
- 
+    centeredSlides={true}
+    autoplay={{
+      delay:2500,
+      disableOnInteraction: false,
+    }}
+    slidesPerView={1} 
+    navigation={true}
+    scrollbar={{draggable: true}}
     loop={true}>
       {slides.map((slide, i) => (
         <SwiperSlide key={i}>
-          <Image src={slide.sourceUrl} alt={slide.altText || `Slide ${i+1}`} 
-          width={300}
-          height={100}
-          priority/>
+          <div className='relative w-full h-64'>
+          <Image src={slide.sourceUrl} 
+          alt={slide.altText || `Slide ${i+1}`} 
+         fill
+          style={{ objectFit: 'cover'}}
+          priority
+          />
+          </div>
         </SwiperSlide>
       ))}
     </Swiper>
